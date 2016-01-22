55c7fd05b37fdbb2fd5e38b93c4d3cb48f4064d9
wobba <miksvenson@gmail.com>
2016-01-22 11:17:31 +0100

    Added description and author info

--- mAdcOW.Search.resultRendered.js
+++ mAdcOW.Search.resultRendered.js
@@ -1,3 +1,12 @@
+/*
+ Author: Mikael Svenson - @mikaelsvenson
+ 
+ Usage:
+
+ Add the script to your page in a script editor web part, content editor web part, reference in the master page, or as a custom action.
+ The clue is that the script should be present and hooked up on the page before the first results are rendered.
+
+*/
 "use strict";
 (function () {
     function hookScript() {
